069f6df2fac6a2ed4723711c8453f131c5ba5d93
larry <26318510+larry0x@users.noreply.github.com>
2022-04-29 15:48:25 +0100

    Display mainnet stats if wallet is not connected

--- store/index.ts
+++ store/index.ts
@@ -52,26 +52,14 @@ export const useStore = create<State>((set) => ({
   unbondRequests: undefined,
 
   update: async (wallet?: ConnectedWallet) => {
-    if (!wallet) {
-      return set({
-        balances: undefined,
-        hubState: undefined,
-        pendingBatch: undefined,
-        unbondRequests: undefined,
-      });
-    }
-
-    if (wallet.network.name !== "mainnet" && wallet.network.name !== "testnet") {
-      throw new Error(`Invalid network ${wallet.network.name}; must be mainnet|testnet`);
-    }
-
-    const network = wallet.network.name as "mainnet" | "testnet";
+    // Display mainnet stats by default
+    const network = wallet ? (wallet.network.name as "mainnet" | "testnet") : "mainnet";
 
     const grpcGatewayUrl = GRPC_GATEWAY_URL[network];
     const { multiquery, steakHub, steakToken } = CONTRACTS[network];
 
-    const accAddress = wallet.terraAddress;
-    const queries1 = encodeBase64([
+    // These are user-independent queries; we query them regardless of whether a wallet is connected
+    let queries: object[] = [
       {
         custom: {
           route: "oracle",
@@ -80,34 +68,6 @@ export const useStore = create<State>((set) => ({
               base_denom: "uluna",
               quote_denoms: ["uusd"],
             },
-          },
-        },
-      },
-      {
-        bank: {
-          balance: {
-            address: accAddress,
-            denom: "uusd",
-          },
-        },
-      },
-      {
-        bank: {
-          balance: {
-            address: accAddress,
-            denom: "uluna",
-          },
-        },
-      },
-      {
-        wasm: {
-          smart: {
-            contract_addr: steakToken,
-            msg: encodeBase64({
-              balance: {
-                address: accAddress,
-              },
-            }),
           },
         },
       },
@@ -141,47 +101,72 @@ export const useStore = create<State>((set) => ({
           },
         },
       },
-      {
-        wasm: {
-          smart: {
-            contract_addr: steakHub,
-            msg: encodeBase64({
-              unbond_requests_by_user: {
-                user: accAddress,
-                limit: 30, // we assume the user doesn't have more than 30 outstanding unbonding requests
-              },
-            }),
+    ];
+
+    // These are user-dependent queries; we query them only if a wallet is connected
+    if (wallet) {
+      queries = queries.concat([
+        {
+          bank: {
+            balance: {
+              address: wallet.terraAddress,
+              denom: "uusd",
+            },
           },
         },
-      },
-    ]);
+        {
+          bank: {
+            balance: {
+              address: wallet.terraAddress,
+              denom: "uluna",
+            },
+          },
+        },
+        {
+          wasm: {
+            smart: {
+              contract_addr: steakToken,
+              msg: encodeBase64({
+                balance: {
+                  address: wallet.terraAddress,
+                },
+              }),
+            },
+          },
+        },
+        {
+          wasm: {
+            smart: {
+              contract_addr: steakHub,
+              msg: encodeBase64({
+                unbond_requests_by_user: {
+                  user: wallet.terraAddress,
+                  limit: 30, // we assume the user doesn't have more than 30 outstanding unbonding requests
+                },
+              }),
+            },
+          },
+        },
+      ]);
+    }
 
-    const axiosResult1 = await axios.get<ContractStoreResponse<MultiqueryResponse>>(
-      `${grpcGatewayUrl}/terra/wasm/v1beta1/contracts/${multiquery}/store?query_msg=${queries1}`
+    let { data } = await axios.get<ContractStoreResponse<MultiqueryResponse>>(
+      `${grpcGatewayUrl}/terra/wasm/v1beta1/contracts/${multiquery}/store?query_msg=${encodeBase64(
+        queries
+      )}`
     );
+
+    // --------------------------- Process user-independent query result ---------------------------
 
     const [
       lunaPriceResult,
-      uusdBalanceResult,
-      ulunaBalanceResult,
-      usteakBalanceResult,
       hubStateResult,
       hubConfigResult,
       pendingBatchResult,
-      unbondRequestsByUserResult,
-    ] = axiosResult1["data"]["query_result"];
+    ] = data["query_result"].slice(0, 4);
 
     if (!lunaPriceResult || !lunaPriceResult.success) {
       throw new Error("Failed to query luna price");
-    }
-    if (!uusdBalanceResult || !uusdBalanceResult.success) {
-      throw new Error("Failed to query uusd balance");
-    }
-    if (!ulunaBalanceResult || !ulunaBalanceResult.success) {
-      throw new Error("Failed to query uluna balance");
-    }
-    if (!usteakBalanceResult || !usteakBalanceResult.success) {
-      throw new Error("Failed to query usteak balance");
     }
     if (!hubStateResult || !hubStateResult.success) {
       throw new Error("Failed to query hub state");
@@ -192,21 +177,53 @@ export const useStore = create<State>((set) => ({
     if (!pendingBatchResult || !pendingBatchResult.success) {
       throw new Error("Failed to query pending batch");
     }
+
+    const lunaPriceResponse: ExchangeRateResponse = decodeBase64(lunaPriceResult.data);
+    const config: ConfigResponse = decodeBase64(hubConfigResult.data);
+    const pendingBatch: PendingBatch = decodeBase64(pendingBatchResult.data);
+    const hubStateResponse: StateResponse = decodeBase64(hubStateResult.data);
+
+    set({
+      priceLunaUsd: Number(lunaPriceResponse["exchange_rates"][0]!["exchange_rate"]),
+      hubState: {
+        totalLunaLocked: Number(hubStateResponse["total_uluna"]) / 1e6,
+        exchangeRate: Number(hubStateResponse["exchange_rate"]),
+      },
+      pendingBatch: {
+        id: pendingBatch.id,
+        startTime: new Date(pendingBatch["est_unbond_start_time"] * 1000),
+      },
+    });
+
+    if (!wallet) { return; }
+
+    // ---------------------------- Process user-dependent query result ----------------------------
+
+    const [
+      uusdBalanceResult,
+      ulunaBalanceResult,
+      usteakBalanceResult,
+      unbondRequestsByUserResult,
+    ] = data["query_result"].slice(4, 8);
+
+    if (!uusdBalanceResult || !uusdBalanceResult.success) {
+      throw new Error("Failed to query uusd balance");
+    }
+    if (!ulunaBalanceResult || !ulunaBalanceResult.success) {
+      throw new Error("Failed to query uluna balance");
+    }
+    if (!usteakBalanceResult || !usteakBalanceResult.success) {
+      throw new Error("Failed to query usteak balance");
+    }
     if (!unbondRequestsByUserResult || !unbondRequestsByUserResult.success) {
       throw new Error(`Failed to query unbonding requests by user ${wallet.terraAddress}`);
     }
 
-    const lunaPriceResponse: ExchangeRateResponse = decodeBase64(lunaPriceResult.data);
     const uusdBalanceResponse: NativeBalanceResponse = decodeBase64(uusdBalanceResult.data);
     const ulunaBalanceResponse: NativeBalanceResponse = decodeBase64(ulunaBalanceResult.data);
     const usteakBalanceResponse: Cw20BalanceResponse = decodeBase64(usteakBalanceResult.data);
-    const hubStateResponse: StateResponse = decodeBase64(hubStateResult.data);
-    const config: ConfigResponse = decodeBase64(hubConfigResult.data);
-    const pendingBatch: PendingBatch = decodeBase64(pendingBatchResult.data);
+    const unbondRequests: UnbondRequestsByUserResponse = decodeBase64(unbondRequestsByUserResult.data);
 
-    const unbondRequests: UnbondRequestsByUserResponse = decodeBase64(
-      unbondRequestsByUserResult.data
-    );
     const ids: number[] = [];
     for (const unbondRequest of unbondRequests) {
       if (unbondRequest.id !== pendingBatch.id) {
@@ -280,19 +297,10 @@ export const useStore = create<State>((set) => ({
     });
 
     set({
-      priceLunaUsd: Number(lunaPriceResponse["exchange_rates"][0]!["exchange_rate"]),
       balances: {
         uusd: Number(uusdBalanceResponse.amount.amount),
         uluna: Number(ulunaBalanceResponse.amount.amount),
         usteak: Number(usteakBalanceResponse.balance),
-      },
-      hubState: {
-        totalLunaLocked: Number(hubStateResponse["total_uluna"]) / 1e6,
-        exchangeRate: Number(hubStateResponse["exchange_rate"]),
-      },
-      pendingBatch: {
-        id: pendingBatch.id,
-        startTime: new Date(pendingBatch["est_unbond_start_time"] * 1000),
       },
       unbondRequests: unbondRequestsParsed,
     });
